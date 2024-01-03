f809151c0df163e6f4ae7c98f3dbf1f1058612f1
dineshncsmg <77341635+dineshncsmg@users.noreply.github.com>
2024-01-03 13:21:01 +0530

    Update about-me.js

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -16,7 +16,7 @@ const AboutMe = () => (
       </div>
           <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
               <div className="about-me">
-                  <p>Web Developer with Around 2 year of experience. Develop highly interactive Front end / User Interfaces for Websites.<br/><br/>
+                  <p>Web Developer with Around 2 years and 6 months of experience. Develop highly interactive Front end / User Interfaces for Websites.<br/><br/>
                     Interested in the entire frontend spectrum and working on ambitious projects. Proficient at Web Application, Website Design, Ecommerce Development and Search Engine Optimization. I have done my graduated Bachelor of Engineering in 2019 Batch</p>
                     <div className="row">
                         <div className="col-md-6">
